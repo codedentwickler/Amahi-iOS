1fd3f74750cf9cdef11d9bf6fd928b014edda846
Kanyinsola Fapohunda Oyindamola <kfapohunda@teamapt.com>
2019-05-04 08:12:36 +0100

    fix: change compressMedia  mimetype to flacMedia

--- AmahiAnywhere/AmahiAnywhere/Presentation/Files/FilesPresenter.swift
+++ AmahiAnywhere/AmahiAnywhere/Presentation/Files/FilesPresenter.swift
@@ -111,7 +111,7 @@ internal class FilesPresenter: BasePresenter {
             self.view?.present(controller)
             break
             
-        case MimeType.video, MimeType.compressedMedia:
+        case MimeType.video, MimeType.flacMedia:
             // TODO: open VideoPlayer and play the file
             guard let url = ServerApi.shared!.getFileUri(file) else {
                 AmahiLogger.log("Invalid file URL, file cannot be opened")
--- AmahiAnywhere/AmahiAnywhere/Utils/Mimes.swift
+++ AmahiAnywhere/AmahiAnywhere/Utils/Mimes.swift
@@ -27,7 +27,7 @@ public class Mimes {
         types.updateValue(MimeType.archive, forKey: "application/x-rar-compressed")
         
         types.updateValue(MimeType.audio, forKey: "application/ogg")
-        types.updateValue(MimeType.compressedMedia, forKey: "application/x-flac")
+        types.updateValue(MimeType.flacMedia, forKey: "application/x-flac")
         
         types.updateValue(MimeType.code, forKey: "text/css")
         types.updateValue(MimeType.code, forKey: "text/html")
@@ -75,17 +75,14 @@ public class Mimes {
         if type != MimeType.undefined {
             return type;
         } else {
-            if isCompressedMedia(mime) {
-                return .compressedMedia
-            }
+            
             return matchCategory(mime)
         }
     }
     
-    private func isCompressedMedia(_ mime: String) -> Bool {
+    private func isFlacMedia(_ mime: String) -> Bool {
         let fileExtensionType = mime.split(separator: "/")[1]
         
-        AmahiLogger.log("\(fileExtensionType)")
         return fileExtensionType == "flac"
     }
     
@@ -98,6 +95,10 @@ public class Mimes {
     
     private func matchCategory(_ mime: String) -> MimeType {
         let type = mime.split(separator: "/")[0]
+        
+        if isFlacMedia(mime) {
+            return .flacMedia
+        }
         
         switch type {
         case "audio":
@@ -115,5 +116,5 @@ public class Mimes {
 }
 
 public enum MimeType: Int {
-    case undefined = 1, archive, audio, code, sharedFile, document, directory, image, presentation, spreadsheet, video, subtitle , compressedMedia
+    case undefined = 1, archive, audio, code, sharedFile, document, directory, image, presentation, spreadsheet, video, subtitle , flacMedia
 }
